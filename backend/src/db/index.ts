47ab0c1caace8232d0a968763f9884e889a77ad2
PancakeMash <arpandevofficial@gmail.com>
2026-07-10 00:57:59 +0100

    wt5 found another missed db setup issue in db-index

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -4,12 +4,8 @@ import postgres from "postgres";
 import * as schema from "./schema.js";
 import { config } from "../config.js";
 
-const conn = postgres({
-  host: config.db.host,
-  port: config.db.port,
-  user: config.db.user,
-  password: config.db.password,
-  database: config.db.database,
+const conn = postgres(config.db.url, {
+  ssl: config.db.ssl,
 });
 
 export const db = drizzle(conn, { schema });
